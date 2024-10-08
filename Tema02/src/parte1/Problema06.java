1366f382ed0cdaf9ae3974ed5c409c73446be644
ja.montano <ja.montano@106-21.info2>
2024-10-08 14:37:42 +0200

    Mejorado el ejercicio 6

--- Tema02/src/parte1/Problema06.java
+++ Tema02/src/parte1/Problema06.java
@@ -8,18 +8,15 @@ public class Problema06 {
 		// Declaramos el escaner
 		Scanner sc = new Scanner(System.in);
 		// Declaramos la variable "a", donde vamos a guardar el primer valor establecido
-		double a;
+		int a;
 		// Declaramos la variable "b", donde vamos a guardar el segundo valor
 		// establecido
-		double b;
+		int b;
 		// Declaramos la variable "c", donde vamos a guardar el tercer valor establecido
-		double c;
+		int c;
 		// Declaramos la variable "operacion", donde vamos a guardar el resultado de la
 		// primera parte de la ecuación
-		double operacion;
-		// Declaramos la variable raiz, donde vamos a guardar el resultado de la raíz
-		// cuadrada de "operacion"
-		double raiz;
+		int operacion;
 		// Declaramos la variable "solucion1", donde vamos a guardar el valor de la
 		// primera solución de la ecuación
 		double solucion1;
@@ -28,21 +25,20 @@ public class Problema06 {
 		double solucion2;
 		// Preguntamos al usuario el valor de a, b y c
 		System.out.println("Dime el valor de a, b y c");
-		a = sc.nextDouble();
-		b = sc.nextDouble();
-		c = sc.nextDouble();
+		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
 		// Comprobomas que "a"distinto de 0 para poder hacer la ecuacion
-		if (a == 0) {// "a" es igual que 0 así que no se puede resolver la ecuación
-			System.out.println("Introduce un valor válido para a");
+		if (a == 0) {// "a" es igual que 0 así que es una ecuación de pimer grado
+			System.out.println("La solución de la ecuación de primer grado es: " + (double) -c/b);
 		} else {// "a" es mayor que 0
 			operacion = (b * b - 4 * a * c);
 			// Comprobamos si el resultado es positivo
 			if (operacion < 0) {// El resultado de la raíz es negativo así que la ecuación no tiene solución
 				System.out.println("La ecuación no se puede resolver");
 			} else {// El resultado de la raíz es positivo así que la ecuación tiene solución
-				raiz = Math.sqrt(operacion);
-				solucion1 = (-(b) + raiz) / (2 * a);
-				solucion2 = (-(b) - raiz) / (2 * a);
+				solucion1 = (double) (-(b) + Math.sqrt(operacion)) / (2 * a);
+				solucion2 = (double) (-(b) - Math.sqrt(operacion)) / (2 * a);
 				// Comprobamos que las soluciones sean distintas
 				if (solucion1 == solucion2) {// La ecuación solo tiene una solución
 					System.out.println("La solución de la ecuación es: " + solucion1);
