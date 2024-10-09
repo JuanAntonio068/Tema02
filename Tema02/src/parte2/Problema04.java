56f3bcb40e266042e942cf3c5a979cf5c83c3bed
Usuario <Usuario@DESKTOP-PARS1KU>
2024-10-09 17:11:53 +0200

    Terminados los comentarios del ejercicio 4 y terminado el ejercicio 5 de la parte 2

--- Tema02/src/parte2/Problema04.java
+++ Tema02/src/parte2/Problema04.java
@@ -13,71 +13,73 @@ public class Problema04 {
 		// Declaramos la variable "tirada2", donde vamos a guardar la segunda tirada del
 		// usuario
 		String tirada2;
-		// Declaramos la variable "num1"
+		// Declaramos la variable "num1", donde vamos a guardar el valor númerico de la
+		// primera tirada
 		int num1;
-
+		// Declaramos la variable "num2", donde vamos a guardar el valor númerico de la
+		// segunda tirada
 		int num2;
-
+		// Preguntamos al usuario cuánto ha sacado en la primera tirada
 		System.out.println("¿Cuánto has sacado en la primera tirada (En formato cadena)");
 		tirada1 = sc.next().toUpperCase();
 
 		num1 = switch (tirada1) {
-		case "UNO" -> {
+		case "UNO" -> { // Si la tirada es 1 le damos a "num1" el valor 1
 			yield 1;
 		}
-		case "DOS" -> {
+		case "DOS" -> { // Si la tirada es 2 le damos a "num1" el valor 2
 			yield 2;
 		}
-		case "TRES" -> {
+		case "TRES" -> { // Si la tirada es 3 le damos a "num1" el valor 3
 			yield 3;
 		}
-		case "CUATRO" -> {
+		case "CUATRO" -> { // Si la tirada es 4 le damos a "num1" el valor 4
 			yield 4;
 		}
-		case "CINCO" -> {
+		case "CINCO" -> { // Si la tirada es 5 le damos a "num1" el valor 5
 			yield 5;
 		}
-		case "SEIS" -> {
+		case "SEIS" -> { // Si la tirada es 5 le damos a "num1" el valor 6
 			yield 6;
 		}
-		default -> {
+		default -> { // El valor introducido es inválido
 			System.out.println("Introduce un valor válido");
-			yield -1;
+			yield -1; // Se le pone -1 indicando el error
 		}
 
 		};
-
+		// Preguntamos al usuario cuánto ha sacado en la segunda tirada
 		System.out.println("¿Cuánto has sacado en la segunda tirada (En formato cadena)");
 		tirada2 = sc.next().toUpperCase();
 
 		num2 = switch (tirada2) {
-		case "UNO" -> {
+		case "UNO" -> { // Si la tirada es 1 le damos a "num2" el valor 1
 			yield 1;
 		}
-		case "DOS" -> {
+		case "DOS" -> { // Si la tirada es 2 le damos a "num2" el valor 2
 			yield 2;
 		}
-		case "TRES" -> {
+		case "TRES" -> { // Si la tirada es 3 le damos a "num2" el valor 3
 			yield 3;
 		}
-		case "CUATRO" -> {
+		case "CUATRO" -> { // Si la tirada es 4 le damos a "num2" el valor 4
 			yield 4;
 		}
-		case "CINCO" -> {
+		case "CINCO" -> { // Si la tirada es 5 le damos a "num2" el valor 5
 			yield 5;
 		}
-		case "SEIS" -> {
+		case "SEIS" -> { // Si la tirada es 6 le damos a "num2" el valor 6
 			yield 6;
 		}
-		default -> {
+		default -> { // El valor introducido es inválido
 			System.out.println("Introduce un valor válido");
-			yield -1;
+			yield -1; // Se le pone -1 indicando el error
 		}
 
 		};
-
+		// Hacemos la suma e imprimimos el resultado
 		System.out.println(num1 + num2);
-
+		// Cerramos el escaner
 		sc.close();
 	}
 
