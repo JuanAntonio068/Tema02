c8f76f15506a5d1b2fe277537439b9431e7d13cf
JuanAntonio068 <ja.montano@iesnervion.es>
2024-10-23 10:16:09 +0200

    Terminado los ejercicios 2, 3, 4, 5, 6 y 7

--- Tema02/src/parte6/Problema01.java
+++ Tema02/src/parte6/Problema01.java
@@ -6,23 +6,26 @@ public class Problema01 {
 
 	public static void main(String[] args) {
 
-		//Declaramos el escaner
+		// Declaramos el escaner
 		Scanner sc = new Scanner(System.in);
 
-		//Declaramos la variable "num", donde vamos a guardar el número escrito por el usuario
+		// Declaramos la variable "num", donde vamos a guardar el número escrito por el
+		// usuario
 		int num;
 
-		//Preguntamos al usuario por un número
+		// Preguntamos al usuario por un número
 		System.out.println("Dime un número");
 
 		num = sc.nextInt();
 
-		//Iniciamos la variable "i"
+		// Iniciamos la variable for
 		for (int i = 1; i <= num; i++) {
 
+			// Imprimimos la variable "i"
 			System.out.println(i);
 		}
-		
+
+		// Cerramos el escaner
 		sc.close();
 	}
 
