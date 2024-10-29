73bd0b89f44c24edc3d527f6028d45aad66b1b67
Usuario <Usuario@DESKTOP-PARS1KU>
2024-10-29 17:40:12 +0100

    Terminaod el ejercicio 8 y 9

--- Tema02/src/parte7/Problema08.java
+++ Tema02/src/parte7/Problema08.java
@@ -1,12 +1,22 @@
 package parte7;
 
 import java.util.InputMismatchException;
+
 import java.util.Scanner;
 
 public class Problema08 {
 
 	public static void main(String[] args) {
+		// Prueba 1
+		// Valor de entrada: 3, 6, 7, 8, 2, 0
+		// Resultado esperado: 5 números introducidos y un fallo
+		// Resultado obtenido: 5 números introducidos y un fallo
 
+		// Prueba 2
+		// Valor de entrada: 50, 23, 23, 1, 2, 3, 4, 5, 0 
+		// Resultado esperado: 8 números introducidos y 7 fallos
+		// Resultado obtenido: 8 números introducidos y 7 fallos
+		
 		// Declaramos el escaner
 		Scanner sc = new Scanner(System.in);
 
@@ -15,7 +25,7 @@ public class Problema08 {
 		int num = 0;
 
 		//Iniciamos la variable "numNuevo"
-		int numNuevo = 0;
+		int numNuevo = 1;
 		
 		// Iniciamos la variable "cont", donde vamos a ir contando los números
 		// introducidos
@@ -49,11 +59,28 @@ public class Problema08 {
 
 		} while (error);
 
-		while (num > 0) {
+		while (numNuevo != 0) {//El bluce donde el juego procede, el cuál no se detiene hasta que el usuario introduzca un 0
 			
-			System.out.println("Dime otro número");
+			//Preguntamos al usuario por otro número
+			System.out.println("Dime otro número mayor");
+			numNuevo = sc.nextInt();
 			
+			if (numNuevo > num || numNuevo == 0) {//El número es correcto o es 0
+				num = numNuevo;
+			}
+			else {//El número es incorrecto
+				System.out.println("Has fallado, el número introducido no es mayor");
+				
+				numFallados++;
+			}
+			cont++;
 		}
+		//Imprimimos el resultado
+		System.out.println("Has introducido " + cont + " números");
+		System.out.println("Has introducido " + numFallados + " intentos fallidos");
+		
+		//Cerramos el escaner
+		sc.close();
 	}
 
 }
--- Tema02/src/parte7/Problema09.java
+++ Tema02/src/parte7/Problema09.java
@@ -3,28 +3,29 @@ package parte7;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Problema07 {
+public class Problema09 {
 
 	public static void main(String[] args) {
 
 		// Prueba 1
-		// Valor de entrada: 4
-		// Resultado esperado: 1
-		//					   121
-		//					   12321
-		//					   1234321
-		// Resultado obtenido: 1
-		//					   121
-		//					   12321
-		//					   1234321
+		// Valor de entrada: 1
+		// Resultado esperado: 1 dígito
+		// Resultado obtenido: 1 dígito
+
 		// Prueba 2
-		// Valor de entrada: 3
-		// Resultado esperado: 1
-		//					   121
-		//					   12321
-		// Resultado obtenido: 1
-		//					   121
-		//					   12321
+		// Valor de entrada: 10 
+		// Resultado esperado: 2 dígitos
+		// Resultado obtenido: 2 dígitos
+		
+		// Prueba 3
+		// Valor de entrada: 1000
+		// Resultado esperado: 3 dígitos
+		// Resultado obtenido: 3 dígitos
+
+		// Prueba 4
+		// Valor de entrada: -1
+		// Resultado esperado: El número tiene que ser mayor o igual que 0
+		// Resultado obtenido: El número tiene que ser mayor o igual que 0
 		
 		// Declaramos el escaner
 		Scanner sc = new Scanner(System.in);
@@ -37,14 +38,17 @@ public class Problema07 {
 		// introducido es correcto
 		boolean error = false;
 
-		// Bucle, donde vamos a vereficar que el valor es correcto
+		// Iniciamos la variable "digitos", donde vamos a ir contando los dígitos que
+		// tenga el número
+		int digitos = 0;
+
 		do {
 			try {
-
 				// Preguntamos al usuario por un número
 				System.out.println("Dime un número");
 				num = sc.nextInt();
 
+				assert num >= 0 : "El número tiene que ser mayor o igual que 0";
 				error = false;
 
 			} catch (InputMismatchException e) {// El valor es incorrecto
@@ -57,22 +61,14 @@ public class Problema07 {
 
 		} while (error);
 
-		// Bucle donde se va a construir la pirámide
-		for (int i = 1; i <= num; i++) {
-			// Bucle donde se va a construir la parte izquierda de la pirámide
+		while (num != 0) {
+			num /= 10;
 
-			for (int j = 1; j <= i; j++) {
-				System.out.print(j);
-			}
-
-			// Bucle donde se va a construir la parte derecha de la pirámide
-			for (int j = i - 1; j >= 1; j--) {
-				System.out.print(j);
-			}
-
-			// Salto de línea
-			System.out.println();
+			digitos++;
 		}
+
+		// Imprimimos el resultado
+		System.out.println("El número que has introducido tiene " + digitos + " dígitos");
 
 		// Cerramos el escaner
 		sc.close();
